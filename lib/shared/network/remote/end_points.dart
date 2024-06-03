d33bd43913fbfe8327efcf6f2e8f8e230c76d979
A-RR3 <arwaarafeh@hotmail.com>
2024-06-03 09:28:32 +0300

    end points/dio changes

--- lib/shared/network/remote/end_points.dart
+++ lib/shared/network/remote/end_points.dart
@@ -1,4 +1,3 @@
-
 const LOGIN = 'login';
 
 const USERS = 'users';
@@ -6,9 +5,9 @@ const USERS = 'users';
 const HOME = 'home';
 
 class EndPointsConstants {
-  static const String subBaseUrl = 'http://192.168.1.10/BFO/palestine_commercial_directory_laravel';
-  static const String subBaseUrl = 'http://192.168.1.4/laravel_project';
-  
+  // static const String subBaseUrl = 'http://192.168.1.10/BFO/palestine_commercial_directory_laravel';
+  static const String subBaseUrl = 'http://192.168.1.9/laravel_project';
+
   static const String baseUrl = '$subBaseUrl/public/api/';
   static const String post = 'post';
 }
